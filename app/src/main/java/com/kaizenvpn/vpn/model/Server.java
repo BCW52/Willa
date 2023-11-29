e5134a39c90bbe5be191235146cb9f932d5b8d0b
bcw52 <nahidislam420gamer@gmail.com>
2023-11-29 20:02:39 +0600

    1

--- app/src/main/java/com/kaizenvpn/vpn/model/Server.java
+++ app/src/main/java/com/kaizenvpn/vpn/model/Server.java
@@ -18,29 +18,12 @@ public class Server {
     }
 
     public Server(String country, String flagUrl, String ovpn, String ovpnUserName, String ovpnUserPassword) {
-        this.country = country;
-        this.flagUrl = flagUrl;
         this.ovpn = ovpn;
         this.ovpnUserName = ovpnUserName;
         this.ovpnUserPassword = ovpnUserPassword;
     }
 
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getFlagUrl() {
-        return flagUrl;
-    }
-
-    public void setFlagUrl(String flagUrl) {
-        this.flagUrl = flagUrl;
-    }
-
+    
     public String getOvpn() {
         return ovpn;
     }
